a35b12be46cb60dcac1eaf0f694023699ee09566
Ariel Pereira <112034805+arielmpereira@users.noreply.github.com>
2022-11-14 22:58:56 -0300

    Update main.java

--- SopaDeLetras/src/Sopa/main.java
+++ SopaDeLetras/src/Sopa/main.java
@@ -5,12 +5,24 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Sopa sopa = new Sopa();
-	Scanner Entrada = new Scanner(System.in);
-       banner(Entrada);
+        String lineaTexto; 
+        Scanner Entrada = new Scanner(System.in);
+                
+        // Imprimimos la pantalla inicial
+        banner(Entrada);
         
+        // Solicitamos las palabras para armar la sopa de letras
+        System.out.println("Ingresa las palabras de tu sopa de letras");
+        System.out.println("Podes ingresar hasta 15 palabras, separadas por comas");
+        System.out.println("Cada palabra puede tener una longitud máxima de 14 caracteres");
+        lineaTexto = Entrada.nextLine();
+        
+        // Creamos la sopa de letras
+        Sopa sopa = new Sopa(lineaTexto);
     }
     
+    // Metodo: banner
+    // Imprime la pantalla inicial del programa
     static void banner(Scanner Entrada) {
         System.out.println("____ ____ ___  ____    ___  ____");
         System.out.println("[__  |  | |__] |__|    |  \\ |___");
