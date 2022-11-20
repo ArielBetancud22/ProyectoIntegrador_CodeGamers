8b4228a09cac990497396f365e7f1ef05d782e39
Diego Di Stefano <92904868+diegodistefano@users.noreply.github.com>
2022-11-20 16:33:20 -0300

    Update Palabra.java

--- SopaDeLetras/src/Sopa/Palabra.java
+++ SopaDeLetras/src/Sopa/Palabra.java
@@ -4,8 +4,8 @@ package Sopa;
 
 public class Palabra {
     public String palabra;
-    public int posx;
-    public int posy;
+    public int posx; //Ingreso en posicion X
+    public int posy; //Ingreso en posicion Y
     public boolean sentidoH;
     public boolean encontrada = false;
     
