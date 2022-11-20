29aff4699737f97831a48922e5fde53c015c9003
Ariel Pereira <112034805+arielmpereira@users.noreply.github.com>
2022-11-20 16:11:33 -0300

    Add files via upload

---
+++ SopaDeLetras/src/Sopa/Jugar.java
@@ -0,0 +1,36 @@
+
+package Sopa;
+
+
+public class Jugar {
+    public Sopa sopa;
+    
+    Jugar(Sopa sopa) {
+        this.sopa = sopa;
+    }
+    
+    /*
+    Metodo cantEncontradas
+    Contabiliza la cantidad de palabras de la sopa de letras
+    que aun no fueron encontradas
+    */
+    public int cantEncontradas() {
+        
+        int cantEncontradas = 0;
+        for (int i = 0; i < sopa.Palabras.length; i++) {
+            if(sopa.Palabras[i].encontrada == true)
+                cantEncontradas ++;
+        }
+        return cantEncontradas;
+    }
+
+    boolean verificarPalabra(String palabraEncontrada) {
+        for (int i = 0; i < sopa.Palabras.length; i++) {
+            if(sopa.Palabras[i].palabra.equals(palabraEncontrada)) {
+                sopa.Palabras[i].encontrada = true;
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ SopaDeLetras/src/Sopa/Palabra.java
@@ -0,0 +1,15 @@
+
+package Sopa;
+
+
+public class Palabra {
+    public String palabra;
+    public int posx;
+    public int posy;
+    public boolean sentidoH;
+    public boolean encontrada = false;
+    
+    public Palabra(String palabra) {
+        this.palabra = palabra;
+    }
+}
